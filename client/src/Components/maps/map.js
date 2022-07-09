6e858048bbbbab8f5bc4e334298fefa7e69e745b
Veda H Mata <hmata.veda@gmail.com>
2022-07-08 23:13:31 -0400

    api keys

--- client/src/Components/maps/map.js
+++ client/src/Components/maps/map.js
@@ -2,19 +2,21 @@ import React from "react";
 import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 import { useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
+
 import "./map.css";
 
 function Map() {
   const { location } = useSelector((state) => state.user);
   const { state } = useLocation();
-  console.log("disMap", state);
+  const apiKey = process.env.REACT_APP_API_KEY;
   const center = {
     lat: location.lat || 38.8115756,
     lng: location.lng || -77.1361142,
   };
-  console.log("lo", center);
+  console.log("lo", apiKey);
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
+    googleMapsApiKey: apiKey,
   });
   if (!isLoaded) {
     return (
